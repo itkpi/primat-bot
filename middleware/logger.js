02d89bfea443b938edc02e47aa394083b669e4a1
FowiFlowi <fowiflowi@gmail.com>
2018-01-22 14:07:02 +0200

    fixed edit event logging

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,11 +2,11 @@ module.exports = (ctx, next) => {
   if (ctx.session && ctx.session.registry) {
     console.log(`[registry] ${ctx.from.username}: ${ctx.message.text}`)
     console.log(ctx.session.registry)
-  } else if (ctx.session && ctx.session.user) {
+  } else if (ctx.session && ctx.session.user && ctx.message) {
     const { username, tgId, group } = ctx.session.user,
           route = config.routes.reduce((res, route) => res || ctx.session[route] && route || null, null)
 
-    const usrTxt = Object.values(config.btns).includes(ctx.message ? ctx.message.text : '')
+    const usrTxt = Object.values(config.btns).includes(ctx.message.text)
                       ? ctx.message.text.slice(3)
                       : ctx.message.text ? ctx.message.text : 'or uploaded smth'
 
