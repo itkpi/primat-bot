79a6d0075324c21d44a62311835e691b5ae7fefd
FowiFlowi <fowiflowi@gmail.com>
2018-02-08 01:05:42 +0200

    added kpi internets list and abitura icons

--- router/abitura/kpi-internets.js
+++ router/abitura/kpi-internets.js
@@ -1,3 +1,7 @@
 module.exports = ctx => {
-  ctx.reply('Тут будет списочек')
+  const links = Object.keys(config.kpi_internets_links)
+
+  const answer = links.map(link => `${link} - ${config.kpi_internets_links[link]}\n\n`)
+                      .join('')
+  return ctx.reply(answer)
 }
