e6a2f6a9877168c7a8403de8149ce30d048ca881
FowiFlowi <fowiflowi@gmail.com>
2018-07-10 15:59:59 +0300

    fixed timetable api route

--- src/route/api.js
+++ src/route/api.js
@@ -4,7 +4,9 @@ const rozklad = require('node-rozklad-api')
 const api = new Router()
 
 module.exports = router => {
-  api.get('/timetable/:group', async ctx => rozklad.timetable(ctx.params.group))
+  api.get('/timetable/:group', async ctx => {
+    ctx.body = await rozklad.timetable(ctx.params.group)
+  })
   // api.get('/timetable')
   router.use('/api', api.routes(), api.allowedMethods())
 }
