19fb408e21e2ece7453db7869b6a606234bf8f57
FowiFlowi <fowiflowi@gmail.com>
2018-07-10 15:55:01 +0300

    added timetable api route

--- src/route/api.js
+++ src/route/api.js
@@ -1,11 +1,10 @@
 const Router = require('koa-router')
+const rozklad = require('node-rozklad-api')
 
 const api = new Router()
 
 module.exports = router => {
-  // api.get('/', ctx => {
-  //   ctx.body =
-  // })
+  api.get('/timetable/:group', async ctx => rozklad.timetable(ctx.params.group))
   // api.get('/timetable')
   router.use('/api', api.routes(), api.allowedMethods())
 }
