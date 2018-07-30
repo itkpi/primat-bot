996c819c72db9905e2b1b81c872d8c36cc968b33
FowiFlowi <fowiflowi@gmail.com>
2018-07-30 15:34:03 +0300

    auth api fix

--- src/service/auth.js
+++ src/service/auth.js
@@ -1,12 +1,15 @@
-const userService = require('../bot/service/user')
+const sessionService = require('../bot/service/session')
 const User = require('../db/models/user')
+const { telegram } = require('../modules/telegraf')
+const getRegMsg = require('../utils/getRegMsg')
 
 module.exports = {
   async register(userData) {
     userData.registeredWithSite = true
     const user = new User(userData)
     await user.save()
-    await userService.finishRegistry(user)
+    await sessionService.setByUser(user)
+    telegram.sendMessage(user.tgId, getRegMsg(user.role))
     return user
   },
 }
